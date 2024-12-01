dc2916dba496c44fb8c8839469b5ef3ecbbd2bb5
Novikov Denis <dn0500625@gmail.com>
2024-12-01 13:14:01 +0300

    смена swagger

--- Program.cs
+++ Program.cs
@@ -48,10 +48,11 @@ var app = builder.Build();
 
 
 
-
-app.UseSwagger();
-app.UseSwaggerUI();
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.UseCookiePolicy(new CookiePolicyOptions()
 { 
     MinimumSameSitePolicy = SameSiteMode.Strict,
